dd9ce38732cdc2e6bf4aca3e964807711b979a9d
thothlogos <thothlogos@protonmail.com>
2023-01-28 17:24:15 -0500

    Basic reply

--- main.py
+++ main.py
@@ -16,32 +16,33 @@ def main():
 
   for unread in reversed(list(reddit.inbox.unread())):
     parse_summon_request(unread)
-    reply(unread)
-    #unread.mark_read()
-
 
 # Format we want to reply to: [ignored] /u/TranslateEnglishFrom [language] [ignored]
 # TODO: You should have to say please? We shouldn't abandon civility towards machines. #Prepare
 def parse_summon_request(message):
   if f"/u/{config.USERNAME}" in message.body:
-    print("[SUMMON] /u/summoned")
+    print("[SUMMON]  /u/summoned")
     lang_capture_attempt = re.search('(\/u\/TranslateEnglishFrom) (\w+)', message.body)
-    if lang_capture_attempt != None:
-      print(f"[CAPTURE] {lang_capture_attempt.group(0)}")
+    if lang_capture_attempt:
+      print(f"[CAPTURE] Raw capture: {lang_capture_attempt.group(0)}")
+      requested_lang = lang_capture_attempt.group(0).split(' ')[1]
+      if requested_lang in config.LANGUAGES:
+        print(f"[CAPTURE] FOUND! -- requested language {requested_lang} is valid")
+        reply_valid_request(message, requested_lang)
+      else:
+        print(f"[ERROR] NOT FOUND -- Requested language: {requested_lang}")
+        #reply_invalid_request(message, requested_lang)
     else:
       print("[ERROR] Language capture regex failed")
-
-    #words = message.body.strip().split()
-    #for lang in config.LANGUAGES:
-    #  if lang in words:
-    #    print(f"[SUMMON] Found requested language: {lang}")
-    #    return lang
   else:
-    print(f"[IGNORED] Message from {message.author} was not a tag")
+    print(f"[IGNORED] Message from {message.author} was not a tag, marking as read")
+    #message.mark_read()
     return
 
 
-def reply(message):
+def reply_valid_request(message, language):
+  print(f"[REPLY]  Attempting to reply to {message.author} for valid {language} request")
+  message.reply(f"You requested a translation of {language}")
   return
 
 def attempt_translation(post):
@@ -50,5 +51,3 @@ def attempt_translation(post):
 
 if __name__ == "__main__":
   main()
-
-
