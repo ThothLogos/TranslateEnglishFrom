350a46d8824cc451331d506a28dca33303a9896f
thothlogos <thothlogos@protonmail.com>
2023-01-28 14:28:21 -0500

    Notice message tags

--- main.py
+++ main.py
@@ -4,13 +4,37 @@ import praw
 import config
 
 
-reddit = praw.Reddit(
-    client_id=config.CLIENT_ID,
-    client_secret=config.CLIENT_SECRET,
-    password=config.PASSWORD,
-    user_agent=config.USER_AGENT,
-    username=config.USERNAME,
-)
+def main():
+  reddit = praw.Reddit(
+      client_id=config.CLIENT_ID,
+      client_secret=config.CLIENT_SECRET,
+      password=config.PASSWORD,
+      user_agent=config.USER_AGENT,
+      username=config.USERNAME,
+  )
 
-for moderator in reddit.subreddit("news").moderator():
-    print(moderator)
+  for unread in reversed(list(reddit.inbox.unread())):
+    print(f"Replying to: [{unread.subject}] {unread.body}")
+    prase_summon_request(unread.body)
+    reply(unread)
+    #unread.mark_read()
+
+
+# Format we want to reply to: [ignored] /u/TranslateEnglishFrom [language] [ignored]
+def prase_summon_request(message):
+  print(f"Incoming body: {message}")
+  message = message.strip()
+  if 'TranslateEnglishFrom' in message:
+    print("Name was tagged")
+  if '/u/TranslateEnglishFrom' in message:
+    print("Name was /u/tagged")
+  return
+
+
+def reply(message):
+  return
+
+if __name__ == "__main__":
+  main()
+
+
