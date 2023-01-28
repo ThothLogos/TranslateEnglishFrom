b525ff4cb48a5f460220a06c04b13cbd431cb839
thothlogos <thothlogos@protonmail.com>
2023-01-28 16:36:28 -0500

    Initial language capture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append('./config')
+import re
 import praw
 import config
 
@@ -14,25 +15,38 @@ def main():
   )
 
   for unread in reversed(list(reddit.inbox.unread())):
-    print(f"Replying to: [{unread.subject}] {unread.body}")
-    prase_summon_request(unread.body)
+    parse_summon_request(unread)
     reply(unread)
     #unread.mark_read()
 
 
 # Format we want to reply to: [ignored] /u/TranslateEnglishFrom [language] [ignored]
-def prase_summon_request(message):
-  print(f"Incoming body: {message}")
-  message = message.strip()
-  if 'TranslateEnglishFrom' in message:
-    print("Name was tagged")
-  if '/u/TranslateEnglishFrom' in message:
-    print("Name was /u/tagged")
-  return
+# TODO: You should have to say please? We shouldn't abandon civility towards machines. #Prepare
+def parse_summon_request(message):
+  if f"/u/{config.USERNAME}" in message.body:
+    print("[SUMMON] /u/summoned")
+    lang_capture_attempt = re.search('(\/u\/TranslateEnglishFrom) (\w+)', message.body)
+    if lang_capture_attempt != None:
+      print(f"[CAPTURE] {lang_capture_attempt.group(0)}")
+    else:
+      print("[ERROR] Language capture regex failed")
+
+    #words = message.body.strip().split()
+    #for lang in config.LANGUAGES:
+    #  if lang in words:
+    #    print(f"[SUMMON] Found requested language: {lang}")
+    #    return lang
+  else:
+    print(f"[IGNORED] Message from {message.author} was not a tag")
+    return
 
 
 def reply(message):
   return
+
+def attempt_translation(post):
+  return
+
 
 if __name__ == "__main__":
   main()
